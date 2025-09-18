5d60df0b617ba4cf4b8687c3963e1de316a18632
Nathan-Crypt0 <nlphillips@byui.edu>
2025-09-18 10:53:21 -0700

    accidentalchangebond

--- csharp-prep/Prep1/JamesBond.cs
+++ csharp-prep/Prep1/JamesBond.cs
@@ -5,12 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("What is your first name?");
-        string first = Console.ReadLine();
+        string firstName = Console.ReadLine();
 
         Console.Write("What is your last name?");
-        string last = Console.ReadLine();
+        string lastName = Console.ReadLine();
 
-        Console.Write($"Your name is {last}, {first} {last}.");
+        Console.Write($"Your name is {lastName}, {firstName} {lastName}.");
 
     }
 }
