395ee09911c8933a6eba36e4f36b8d6baa2d2179
Nathan Phillips <nlphillips01@gmail.com>
2025-10-10 13:10:05 -0600

    journal1-4

--- prove/Develop02/RandomPromptGenerator.cs
+++ prove/Develop02/RandomPromptGenerator.cs
@@ -1,23 +1,29 @@
-// using System;
-// using System.Collections.Generic;
-// public class SentencePicker
+using System;
+using System.Collections.Generic;
 
-// {
-//     public void SentenceList(string sentences);
-//     string[] sentences =
-//     {
-//         "What is on your mind today?",
+public class SentencePicker
+{
+    public void PickSentence()
+    {
+        string[] sentences =
+        {
+            "What is on your mind today?",
+            "Welcome to your private journal.",
+            "How are you feeling right now?",
+            "Write about something good that happened today.",
+            "What do you want to improve this week?",
+            "Describe your favorite memory.",
+            "What’s something you learned recently?",
+            "If you could change one thing about today, what would it be?",
+            "Who made you smile today?",
+            "What’s a goal you want to work on?"
+        };
 
-//         "Welcome to your private journal",
-//     };
+        Random random = new Random();
+        int randomIndex = random.Next(sentences.Length);
 
-//     Random random = new Random();
+        string randomSentence = sentences[randomIndex];
+        Console.WriteLine(randomSentence);
+    }
+}
 
-//     int randomIndex = Random.Next(sentences.Length);
-//     string randomSentence = sentences[randomIndex];
-
-    
-    
-
-
-// }
--- prove/Develop02/SavefileLoadfile.cs
+++ prove/Develop02/SavefileLoadfile.cs
@@ -1,9 +1,31 @@
 using System;
+using System.IO;
 
-class File
+class SaveLoadFile
 {
-    
-    
-        
-    
+    static void Main()
+    {
+        string filename = "MyJournalFiles.txt";
+
+        // Write to file
+        using (StreamWriter outputFile = new StreamWriter(filename))
+        {
+            // what i want to save and load here
+            outputFile.WriteLine();
+            outputFile.WriteLine();
+        }
+
+        // Read from file
+        string[] lines = File.ReadAllLines(filename);
+
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split(',');
+
+            string firstName = parts[0];
+            string lastName = parts[1];
+
+            Console.WriteLine($"First name: {firstName}, Last name: {lastName}");
+        }
+    }
 }
